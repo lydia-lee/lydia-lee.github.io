425460b5f67beeb11b025d9552418e17b550e314
Lydia Lee <lydia.lee@berkeley.edu>
2019-09-19 22:26:42 -0700

    [ll] Removing most old content

--- old/base/navbar.js
+++
@@ -1,9 +0,0 @@
-document.write(`<div class="w3-top">
-  <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
-    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-    <a href="index.html" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Home</a>
-    <a href="research.html" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Research</a>
-    <a href="teaching.html" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Teaching</a>
-    <a href="photos.html" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Photography</a>
-  </div>
-</div>`)
